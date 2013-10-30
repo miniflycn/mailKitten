21222a21def62cc4b8dffe8198f9b94b38bd8908
miniflycn <miniflycn@justany.net>
2013-10-30 10:26:46 +0800

    finish submit validate

--- www/js/ui.js
+++ www/js/ui.js
@@ -29,46 +29,73 @@
 	});
 
 	UI.lang = {};
-
-	UI.validate = function () {
-		$(document).on('focusout', 'input[data-validate]', function (event) {
-			var ele = $(event.target),
-				valids = $(ele).attr('data-validate').split(/\s+/),
-				value = ele.val(),
-				group = ele.closest('.form-group'),
-				helper = $('.help-error', group),
-				len = valids.length,
-				valid;
-
-			for (var i = 0; i < len; i++) {
-				valid = valids[i];
-				if (!UI.validate[valid](value)) {
-					group.addClass('has-error');
-					helper.html(UI.lang.validate[valid]);
-					return;
-				}
-			}
-			group.removeClass('has-error');
-			helper.empty();
-		});
-	};
-	$.extend(UI.validate, {
-		isEmail: (/^[\w\.]+@([a-zA-Z0-9\-]{1,63}\.)+[a-zA-Z0-9\-]{1,63}$/),
-		email: function (value) {
-			if (!$.trim(value)) {
-				return true;
-			}
-			value = $.trim(value.toLowerCase());
-			return this.isEmail.test(value);
-		},
-		mandatory: function (value) {
-			return !!value;
-		}
-	});
 	UI.lang.validate = {
 		email: 'Invalid email address.',
 		mandatory: 'Input required.'
 	};
+
+	!function (UI) {
+		function validate() {
+			$(document).on('focusout', 'input[data-validate]', function (event) {
+				return one(event.target);
+			});
+		}
+
+		function one(ele) {
+			ele = $(ele);
+			var valids = ele.attr('data-validate').split(/\s+/),
+				value = ele.val(),
+				group = ele.closest('.form-group'),
+				helper = $('.help-error', group),
+				len = valids.length,
+				i = 0,
+				valid;
+
+			for (; i < len; i++) {
+				valid = valids[i];
+				if (!UI.validate[valid](value)) {
+					group.addClass('has-error');
+					helper.html(UI.lang.validate[valid]);
+					return false;
+				}
+			}
+			group.removeClass('has-error');
+			helper.empty();
+			return true;
+		}
+
+		function all(container) {
+			var _$ = $('input[data-validate]', container),
+				len = _$.length,
+				i = 0,
+				passed = true;
+			for (; i < len; i++) {
+				if (!one(_$[i])) {
+					passed = false;
+				}
+			}
+			return passed;
+		}
+
+
+		$.extend(validate, {
+			isEmail: (/^[\w\.]+@([a-zA-Z0-9\-]{1,63}\.)+[a-zA-Z0-9\-]{1,63}$/),
+			email: function (value) {
+				if (!$.trim(value)) {
+					return true;
+				}
+				value = $.trim(value.toLowerCase());
+				return this.isEmail.test(value);
+			},
+			mandatory: function (value) {
+				return !!value;
+			},
+			one: one,
+			all: all
+		});
+
+		UI.validate = validate;
+	}(UI);
 
 	// Init	
 	$(document).ready(function () {
