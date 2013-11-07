bd747c0b5603d81b7c46a7e8c0b4a082425da068
miniflycn <miniflycn@justany.net>
2013-11-07 17:47:46 +0800

    OOP design

---
+++ src/js/ui/Model.js
@@ -0,0 +1,26 @@
+!function (UI, $) {
+
+	function Model() {
+
+	}
+
+	$.extend(Model, {
+		// 创建
+		create: function () {
+
+		},
+		// 继承
+		extend: function () {
+
+		},
+		// 组合
+		merge: function (instance, bind) {
+			if (bind) {
+
+			} else {
+
+			}
+		}
+	});
+
+}(UI, $)
